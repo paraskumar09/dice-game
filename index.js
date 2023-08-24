d98dae67eb8d4ed570b6c3f60e0f8a4a093396f2
paras kumar <parasyadav.py02@gmail.com>
2023-08-24 22:05:29 +0530

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+var name1=prompt("enter your name:");
+var name2=prompt("enter your name:");
+
+var num=Math.ceil(Math.random()*6);
+var numImg="dice"+num+".png";
+var numImgSrc="images/"+numImg;
+document.querySelector(".img1").setAttribute("src",numImgSrc);
+
+
+var num2=Math.ceil(Math.random()*6);
+var num2Img="dice"+num2+".png";
+var num2ImgSrc="images/"+num2Img;
+document.querySelector(".img2 ").setAttribute("src",num2ImgSrc);
+
+
+if(num>num2)
+{
+    document.querySelector(".heading").innerHTML=name1+" is chutia";
+
+}
+else if(num2>num)
+{
+    document.querySelector(".heading").innerHTML=name2+" is chutia";
+}
+else
+{
+    document.querySelector(".heading").innerHTML="both "+name1+" and "+name2+ "are chutia";
+}
